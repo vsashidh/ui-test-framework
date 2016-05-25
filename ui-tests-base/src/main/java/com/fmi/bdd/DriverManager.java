92b23a7ee9026053662346822eaa2e54ce709caa
Vivek Sashidharan <vsashidharan@foundationmedicine.com>
2016-05-25 18:38:13 -0400

    Added new application properties

--- ui-tests-base/src/main/java/com/fmi/bdd/DriverManager.java
+++ ui-tests-base/src/main/java/com/fmi/bdd/DriverManager.java
@@ -1,35 +1,52 @@
 package com.fmi.bdd;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.fmi.bdd.Driver.DriverType;
 
 //@Component
 //@Scope("cucumber-glue")
 public class DriverManager {
 	// making the class a singleton
-//	@Value("browser")
-	private String type = "chrome";
+	// @Value("browser")
+	public enum BrowserType {
+		CHROME, SAFARI, IE, EDGE
+	};
+
+	private BrowserType brType = BrowserType.CHROME;
+	private DriverType drType = DriverType.WEBDRIVER;
 	private Driver driver;
 
-	private DriverManager(){
+	private DriverManager() {
 		System.out.println("DriverManager has been instantiated");
 	}
-	
+
 	public Driver getProvider() {
 		if (driver == null) {
-			switch (type) {
-			case "chrome":
-				driver = new SeleniumDriver<ChromeDriver>(new ChromeDriver());
-				break;
-			default:
-				driver = null;
-				break;
+			if (drType == DriverType.WEBDRIVER) {
+				switch (brType) {
+				case CHROME:
+					driver = new SeleniumDriver<ChromeDriver>(new ChromeDriver());
+					break;
+				case SAFARI:
+					driver = new SeleniumDriver<SafariDriver>(new SafariDriver());
+					break;
+				default:
+					driver = null;
+					break;
+				}
 			}
 		}
 		return driver;
+	}
+
+	public void setBrType(BrowserType brType) {
+		this.brType = brType;
+	}
+
+	public void setDrType(DriverType drType) {
+		this.drType = drType;
 	};
-	
 
 }
